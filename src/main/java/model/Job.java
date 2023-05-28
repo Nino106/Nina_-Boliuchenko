776b8e285f714be96469243f2ed5430968eb41b9
Nina Boliuchenko <98800606+Nino106@users.noreply.github.com>
2023-05-28 22:03:03 +0300

    Create Job.java

---
+++ src/main/java/model/Job.java
@@ -0,0 +1,10 @@
+package Model;
+
+import lombok.Data;
+
+@Data
+public class Job {
+    private String jobName = "Student";
+    private String jobDescription = "IASA student";
+    private String note = "3rd course";
+}
