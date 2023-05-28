ee2687fd426cd96f594912828eb628a8ecfffd67
Nina Boliuchenko <98800606+Nino106@users.noreply.github.com>
2023-05-28 21:15:10 +0300

    Create Config.java

---
+++ src/main/java/URI/Config.java
@@ -0,0 +1,13 @@
+package URI;
+
+public class Config {
+    public static final String ACCESS_TOKEN = "sl.Bemgf8rO_L5Px9wkx1QaDAW3CCr7CnO8wyfr-iDL7n-BCzHN0gu8PELr5f0WXSKkWHEpn56SD6cVtyh1Dz3FWyfWOlaaChZ0McoIxJMl4TyrEE7JCMGlfe0LOpBOwAc5ilUlt80";
+    public static final String UPLOAD = "https://content.dropboxapi.com/2/files/upload";
+    public static final String METADATA = "https://api.dropboxapi.com/2/files/get_metadata";
+    public static final String DELETE = "https://api.dropboxapi.com/2/files/delete_v2";
+    public static final String CHECK = "https://api.dropboxapi.com/2/files/list_folder";
+    public static final String FILE_PATH = "D:\\Univercity\\SDT\\WEB_API\\src\\main\\java\\DropBoxApi\\test.txt";
+    public static final String FILE_NAME = "test.txt";
+    public static final String DROPBOX_FILEPATH = "apps/efanov_web_api/test.txt";
+    public static final String DROPBOX_PATH = "/apps/efanov_web_api";
+}
